f3c6e3f5d2bb1677418fb3dc8fc781430d491fe8
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-14 08:44:56 +0100

    add: init script

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 WORKDIR=~/home/$USER/server/
 BUILDDIR=~/home/$USER/server/build/
+FRONTDIR=~/home/$USER/server/frontend/
+BACKDIR=~/home/$USER/server/backend/
+DISPDIR=~/home/$USER/server/display/
 
+$BACKDIR/build.sh
+$FRONTDIR/build.sh
+$DISPDIR/build.sh
 
-cd $WORKDIR/backend
-npm i
-cp -r * $BUILDDIR/backend/
-cd $WORKDIR/frontend
-npm i
-npm run build
-cp -r build $BUILDDIR/frontend/
-cd $WORKDIR/display
-npm i
-npm run build
-cp -r build $BUILDDIR/display/
+cp -r $BACKDIR/* $BUILDDIR/backend/
+cp -r $FRONTDIR/build $BUILDDIR/frontend/
+cp -r $DISPDIR/build $BUILDDIR/display/
+
+$BUILDIR/update-repo.sh
