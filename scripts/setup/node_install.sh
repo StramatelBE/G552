3831040454d689873dd5b60a288c7d4d8aaffe3e
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-14 08:37:13 +0100

    add: nvm config to node install

--- scripts/setup/node_install.sh
+++ scripts/setup/node_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-nvm install v20.11.0
 
-bash
+export NVM_DIR=/home/mderoir/.nvm
+[ -s /home/mderoir/.nvm/nvm.sh ] && \. /home/mderoir/.nvm/nvm.sh  # This loads nvm
+[ -s /home/mderoir/.nvm/bash_completion ] && \. /home/mderoir/.nvm/bash_completion  # This loads nvm bash_completion
+
+nvm install v20.11.0
