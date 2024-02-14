8f965a57d7c458dae867161cb4b56ffc84e42598
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-14 09:00:55 +0100

    add: build.sh

--- scripts/setup/npm_init.sh
+++ scripts/setup/npm_init.sh
@@ -8,18 +8,22 @@ BACKDIR=~/server/backend
 DISPDIR=~/server/display
 
 
-
+CUR_POS=$PWD
 
 echo "BACKEND NODE MODULES"
 cp $SCRIPTDIR/init.sh $BACKDIR
+cd $BACKDIR
 $BACKDIR/init.sh
 echo "FRONTEND NODE MODULES"
 cp $SCRIPTDIR/init.sh $FRONTDIR
+cd $FRONTDIR
 $FRONTDIR/init.sh
 echo "DISPLAY NODE MODULES"
 cp $SCRIPTDIR/init.sh $DISPDIR
+cd $DISPDIR
 $DISPDIR/init.sh
 
 echo "### END ###"
+cd $CUR_POS
 
 
