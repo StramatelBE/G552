21ae31ce42a2ceceb5e00849e27bc6268cd69315
mderoir <mderoir@PC-MDR.stramatel.local>
2024-02-26 12:23:57 +0100

    added version and checksum tests

--- create_build.sh
+++ create_build.sh
@@ -2,29 +2,60 @@
 
 read -p "Enter a version number for this build: " VERSION_NUMBER
 
-echo "$VERSION_NUMBER" > metadata/version
+FOLDER=zip/$VERSION_NUMBER
+BUILD=$FOLDER/G552_BUILD_$VERSION_NUMBER.zip
+BUILD_REPO=~/Documents/Stramatel/g552_builds/builds
+METADATA=$FOLDER/metadata
+CHECKSUM=$METADATA/checksum
+VERSION=$METADATA/version
 
-echo "Version $VERSION_NUMBER has been saved."
+function check_duplicate_checksums() {
+    local new_checksum=$(sha256sum "$BUILD" | awk '{print $1}')
+    for dir in zip/*/; do
+        if [ -f "${dir}metadata/checksum" ] && [ "$(cat "${dir}metadata/checksum")" == "$new_checksum" ] && [ "${dir}" != "$FOLDER/" ]; then
+            local existing_version=$(basename "$dir")
+            echo "A similar build with the same checksum exists for version: ${existing_version%/}"
+            exit 1
+        fi
+    done
+}
 
-BUILD=G552_BUILD_$VERSION_NUMBER.zip
-BUILD_REPO=~/Documents/Stramatel/g552_builds
-echo "Building project version: $VERSION_NUMBER."
-bash ./scripts/build/project_build.sh > /dev/null 2>&1
-
-echo "Packaging builds..."
-zip -9 -r zip/$BUILD build/ > /dev/null 2>&1
-
-echo "Creating checksum..."
-sha256sum zip/$BUILD | awk {'print $1'} > metadata/checksum
-truncate --size -1 metadata/checksum
-echo -n "|" >> metadata/checksum
-cat metadata/version >> metadata/checksum
-CHECKSUM=$(cat metadata/checksum)
-echo "Checksum : $CHECKSUM"
-
-echo "Copying files..."
-mkdir $BUILD_REPO/$VERSION_NUMBER
-cp zip/$BUILD $BUILD_REPO/$VERSION_NUMBER
-cp -r metadata/ $BUILD_REPO/$VERSION_NUMBER
-echo "Files copied!"
-
+echo "Creating Version Folder"
+if [ -d "$FOLDER" ]; then
+    echo "This Version directory already exists: $VERSION_NUMBER"
+    if [ -f "$CHECKSUM" ]; then
+        bash ./scripts/build/project_build.sh > /dev/null 2>&1
+        zip -9 -r $BUILD build/ > /dev/null 2>&1
+        NEW_CHECKSUM=$(sha256sum $BUILD | awk '{print $1}')
+        
+        if [ "$NEW_CHECKSUM" != "$(cat $CHECKSUM)" ]; then
+            read -p "Checksums are different. Do you want to erase the existing version? (y/n): " user_choice
+            if [ "$user_choice" == "y" ]; then
+                echo "$NEW_CHECKSUM" > $CHECKSUM
+                echo "$VERSION_NUMBER" > $VERSION
+                cp -r $FOLDER $BUILD_REPO/$VERSION_NUMBER
+                echo "Files copied with new version!"
+            else
+                echo "Exiting script."
+                exit 1
+            fi
+        else
+            echo "Checksums are the same, no need to copy anything."
+            exit 1
+        fi
+    fi
+else
+    mkdir -p $FOLDER
+    mkdir -p $METADATA
+    echo "$VERSION_NUMBER" > $VERSION
+    echo "Building project version: $VERSION_NUMBER."
+    bash ./scripts/build/project_build.sh > /dev/null 2>&1
+    echo "Packaging builds..."
+    zip -9 -r $BUILD build/ > /dev/null 2>&1
+    echo "Creating checksum..."
+    sha256sum $BUILD | awk '{print $1'} > $CHECKSUM
+    check_duplicate_checksums
+    echo "Copying files..."
+    cp -r $FOLDER $BUILD_REPO/$VERSION_NUMBER
+    echo "Files copied!"
+fi
