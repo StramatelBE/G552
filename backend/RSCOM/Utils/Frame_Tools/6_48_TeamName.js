538203bac9ba5dcd29da6097f985d3f8db76c1b1
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-03 11:27:07 +0200

    cross in teamname fixe

--- backend/RSCOM/Utils/Frame_Tools/6_48_TeamName.js
+++ backend/RSCOM/Utils/Frame_Tools/6_48_TeamName.js
@@ -8,6 +8,16 @@ function TeamName(startIndex, _message) {
     // the size is 18/2 because we want to slice 18 bytes, but the buffer is in utf16, so 2 bytes per character
     const slicedBuffer = buffer.slice(startIndex, startIndex + 9 * 2);
 
+     // Replace all occurrences of 20 20 with 00 20 in the sliced buffer
+     // TODO: correct it in the code
+     for (let i = 0; i < slicedBuffer.length - 2; i += 2) {
+        if (slicedBuffer[i] === 0x20 && slicedBuffer[i + 2] === 0x20) {
+            slicedBuffer[i] = 0x00; // Change the first 20 to 00
+            // No need to change the second 20 as it's already 20
+            i += 2; // Move past the modified sequence to avoid overlapping changes
+        }
+    }
+
 
     const decodedMessage = iconv.decode(slicedBuffer, 'utf16');
 
