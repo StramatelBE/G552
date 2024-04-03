5d0017430cf90e112332c130693bd6d329ac9d63
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-04-03 14:43:35 +0200

    Correction de bugs et améliorations de code

--- display/src/Components/Sports/Basketball.js
+++ display/src/Components/Sports/Basketball.js
@@ -1,6 +1,176 @@
 import React, { useEffect, useState } from "react";
 
 function Basketball({ gameState: incomingGameState }) {
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const homeScoreStyle = {
+    width: "226px",
+    height: "100px",
+    left: "0px",
+    top: "0px",
+    position: "absolute",
+    textAlign: "center",
+    color: "#FF0000",
+    fontSize: "100px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const homeNameStyle = {
+    width: "226px",
+    height: "100px",
+    left: "0px",
+    top: "83px",
+    position: "absolute",
+    textAlign: "center",
+    color: "white",
+    fontSize: "45px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const homeTimeoutStyle = {
+    width: "15px",
+    height: "15px",
+    left: "100px",
+    top: "139px",
+    position: "absolute",
+    background: "#00A13B",
+    borderRadius: "9999px",
+  };
+
+  const homeFoulsStyle = {
+    width: "25px",
+    height: "46px",
+    left: "31px",
+    top: "195px",
+    position: "absolute",
+    textAlign: "center",
+    color: "#EBFF00",
+    fontSize: "50px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const homePossessionStyle = {
+    clipPath: "polygon(100% 0%, 82.4% 49.5%, 100% 100%, 0% 50%)",
+    background: "#ff0000",
+    height: "20px",
+    width: "20px",
+    left: "203px",
+    top: "40px",
+    position: "absolute",
+  };
+
+  const periodStyle = {
+    width: "26px",
+    height: "64px",
+    left: "243px",
+    top: "18px",
+    position: "absolute",
+    color: "#00A13B",
+    fontSize: "60px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const timerStyle = {
+    width: "250px",
+    height: "76px",
+    left: "137px",
+    top: "134px",
+    position: "absolute",
+    textAlign: "center",
+    color: "white",
+    fontSize: "95px",
+    fontFamily: '"D-DIN-Bold"',
+    wordWrap: "break-word",
+  };
+
+  const guestNameStyle = {
+    width: "226px",
+    height: "56px",
+    left: "286px",
+    top: "85px",
+    position: "absolute",
+    textAlign: "center",
+    color: "white",
+    fontSize: "45px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const guestScoreStyle = {
+    width: "226px",
+    height: "100px",
+    left: "286px",
+    top: "0px",
+    position: "absolute",
+    textAlign: "center",
+    color: "#FF0000",
+    fontSize: "100px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const guestTimeoutStyle = {
+    width: "15px",
+    height: "15px",
+    left: "398px",
+  };
+
+  const guestFoulsStyle = {
+    width: "25px",
+    height: "46px",
+    left: "456px",
+    top: "195px",
+    position: "absolute",
+    textAlign: "center",
+    color: "#EBFF00",
+    fontSize: "50px",
+    fontFamily: '"D-DIN-Bold"',
+    fontWeight: "700",
+    wordWrap: "break-word",
+  };
+
+  const guestPossessionStyle = {
+    clipPath: "polygon(0% 0%, 100% 50%, 0% 100%, 25% 50%)",
+    background: "#ff0000",
+    height: "20px",
+    width: "20px",
+    left: "293px",
+    top: "40px",
+    position: "absolute",
+  };
+
+  const imageStyle = {
+    width: "130px",
+    height: "21px",
+    left: "191px",
+    top: "230px",
+    position: "absolute",
+  };
+
+  const fibaImageStyle = {
+    width: "32px",
+    height: "65px",
+    left: "450px",
+    top: "130px",
+    position: "absolute",
+  };
 
   function formatTimer(timerString, showHomeTimeout, showGuestTimeout) {
     if (!timerString) {
@@ -18,8 +188,8 @@ function Basketball({ gameState: incomingGameState }) {
       characters.unshift("\u00A0");
     }
 
-    const homeTimeout = gameState.Home.Timeout.Time || "0:00";
-    const guestTimeout = gameState.Guest.Timeout.Time || "0:00";
+    const homeTimeout = gameState?.Home?.Timeout.Time || "0:00";
+    const guestTimeout = gameState?.Guest?.Timeout.Time || "0:00";
 
     if (showHomeTimeout && homeTimeout !== "0:00") {
       return formatTimer(homeTimeout);
@@ -48,52 +218,15 @@ function Basketball({ gameState: incomingGameState }) {
   }
 
   const gameState = incomingGameState || {};
-  const showHomeTimeout = gameState.Home.Timeout.Time !== "0:00";
-  const showGuestTimeout = gameState.Guest.Timeout.Time !== "0:00";
+  const showHomeTimeout = gameState?.Home?.Timeout?.Time !== "0:00";
+  const showGuestTimeout = gameState?.Guest?.Timeout?.Time !== "0:00";
+
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        position: "relative",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          width: "226px",
-          height: "100px",
-          left: "0px",
-          top: "0px",
-          position: "absolute",
-          textAlign: "center",
-          color: "#FF0000",
-          fontSize: "100px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+    <div style={containerStyle}>
+      <div style={homeScoreStyle}>
         {gameState?.Home?.Points || "0"} {/* team score */}
       </div>
-      <div
-        style={{
-
-          width: "226px",
-          height: "100px",
-          left: "0px",
-          top: "83px",
-          position: "absolute",
-          textAlign: "center",
-          color: "white",
-          fontSize: "45px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={homeNameStyle}>
         {gameState?.Home?.TeamName || "HOME"} {/* team name HOME */}
       </div>
 
@@ -116,116 +249,31 @@ function Basketball({ gameState: incomingGameState }) {
         </>
       )}{/* team HOME time out */}
 
-      <div
-        style={{
-          width: "25px",
-          height: "46px",
-          left: "31px",
-          top: "195px",
-          position: "absolute",
-          textAlign: "center",
-          color: "#EBFF00",
-          fontSize: "50px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={homeFoulsStyle}>
         {gameState?.Home?.Fouls?.Team} {/* team HOME fouls */}
       </div>
-      {gameState?.Home?.Possession && <div style={{
-        "clip-path": "polygon(100% 0%, 82.4% 49.5%, 100% 100%, 0% 50%)",
-        background: "#ff0000",
-        height: "20px",
-        width: "20px",
-        left: "203px",
-        top: "40px",
-        position: "absolute",
+      {gameState?.Home?.Possession && (
+        <div style={homePossessionStyle}></div>
+      )}
 
-      }}>
-      </div>
-      }
-
-
-
-      <div
-        style={{
-          width: "26px",
-          height: "64px",
-          left: "243px",
-          top: "18px",
-          position: "absolute",
-          color: "#00A13B",
-          fontSize: "60px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={periodStyle}>
         {gameState?.Period || "0"} {/* period */}
       </div>
-      {gameState?.Guest?.Possession &&
-        <div style={{
-          "clip-path": "polygon(0% 0%, 100% 50%, 0% 100%, 25% 50%)",
-          background: "#ff0000",
-          height: "20px",
-          width: "20px",
-          left: "293px",
-          top: "40px",
-          position: "absolute",
-        }}>
-        </div>
-      }
-      <div
-        style={{
-          width: "250px",
-          height: "76px",
-          left: "137px",
-          top: "134px",
-          position: "absolute",
-          textAlign: "center",
-          color: "white",
-          fontSize: "95px",
-          fontFamily: '"D-DIN-Bold"',
-          wordWrap: "break-word",
-        }}
-      >
-        {formatTimer(gameState?.Timer.Value || "00:00", showHomeTimeout, showGuestTimeout)}
+      {gameState?.Guest?.Possession && (
+        <div style={guestPossessionStyle}></div>
+      )}
+      <div style={timerStyle}>
+        {formatTimer(
+          gameState?.Timer?.Value || "00:00",
+          showHomeTimeout,
+          showGuestTimeout
+        )}
       </div>
-      <div
-        style={{
-          width: "226px",
-          height: "56px",
-          left: "286px",
-          top: "85px",
-          position: "absolute",
-          textAlign: "center",
-          color: "white",
-          fontSize: "45px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={guestNameStyle}>
         {gameState?.Guest?.TeamName || "GUEST"} {/* team name GUEST */}
       </div>{" "}
 
-
-      <div
-        style={{
-          width: "226px",
-          height: "100px",
-          left: "286px",
-          top: "0px",
-          position: "absolute",
-          textAlign: "center",
-          color: "#FF0000",
-          fontSize: "100px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={guestScoreStyle}>
         {gameState?.Guest?.Points || "0"} {/* team score */}
       </div>{" "}
       {gameState?.Guest?.Timeout?.Team > 0 && (
@@ -236,57 +284,23 @@ function Basketball({ gameState: incomingGameState }) {
               style={{
                 width: "15px",
                 height: "15px",
-                left: "398px",
+                left: "100px",
                 top: `${139 + i * 20}px`,
                 position: "absolute",
                 background: "#00A13B",
                 borderRadius: "9999px",
               }}
             />
-          ))
-          }
+          ))}
         </>
       )}{/* team GUEST time out */}
 
-      <div
-        style={{
-          width: "25px",
-          height: "46px",
-          left: "456px",
-          top: "195px",
-          position: "absolute",
-          textAlign: "center",
-          color: "#EBFF00",
-          fontSize: "50px",
-          fontFamily: '"D-DIN-Bold"',
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
+      <div style={guestFoulsStyle}>
         {gameState?.Guest?.Fouls?.Team} {/* team GUEST fouls */}
       </div>
 
-      <img
-        style={{
-          width: "130px",
-          height: "21px",
-          left: "191px",
-          top: "230px",
-          position: "absolute",
-        }}
-        src="LOGO_Stramatel.gif"
-      />{" "}
-      <img
-        style={{
-          width: "32px",
-          height: "65px",
-          left: "450px",
-          top: "130px",
-          position: "absolute",
-        }}
-        src="fiba.png"
-      />{" "}
-
+      <img style={imageStyle} src="LOGO_Stramatel.gif" />{" "}
+      <img style={fibaImageStyle} src="fiba.png" />{" "}
     </div>
   );
 };
