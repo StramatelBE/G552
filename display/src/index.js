0f168fe595357f73160724c4c211870e930dad93
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-05 12:44:04 +0200

    removed Sport type in basketball

--- display/src/index.js
+++ display/src/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import ScoringMode from "./Components/ScoringMode";
+import ScoringMode from "./Components/ScoringMode"; 
 import MediaMode from "./Components/MediaMode";
 import config from "./config.js";
 import LogoMode from "./Components/LogoMode";
-import "./main.css"
+import "./main.css";
 
 const { ipcRenderer } = window.require("electron");
 
@@ -17,112 +17,112 @@ const App = () => {
   const [mediaState, setMediaState] = useState([]);
   const [mediaMode, setMediaMode] = useState(false);
 
-    useEffect(() => {
-        document.documentElement.style.setProperty(
-            "--maxWidth",
-            config.display.width
-        );
-        document.documentElement.style.setProperty(
-            "--maxHeight",
-            config.display.height
-        );
-        console.log("App mounted");
-        ipcRenderer.on("server-data", (event, data) => {
-            console.log('!Received gameState', data, event);
-            if (data.Mode === 9) {
-                setMode("scoring");
-                setGameState(data || {}); // Assuming the data for scoring mode contains a 'gameState' property
-            } else if (data.Mode === 22) {
-                setMode("logo");
-            } else if (data.Mode === 23) {
-                setMode("sleep")
-            } else {
-                let mediaArray = [];
-                setMediaMode(false);
-                setMode("media");
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--maxWidth",
+      config.display.width
+    );
+    document.documentElement.style.setProperty(
+      "--maxHeight",
+      config.display.height
+    );
+    console.log("App mounted");
+    ipcRenderer.on("server-data", (event, data) => {
+      console.log('!Received gameState', data, event);
+      if (data.Mode === 9) {
+        setMode("scoring");
+        setGameState(data || {}); // Assuming the data for scoring mode contains a 'gameState' property
+      } else if (data.Mode === 22) {
+        setMode("logo");
+      } else if (data.Mode === 23) {
+        setMode("sleep")
+      } else {
+        let mediaArray = [];
+        setMediaMode(false);
+        setMode("media");
 
         // if data.medias is not an array, wrap it in one
         switch (data.Mode) {
-          case 0:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_DEFENCE.mp4",
-                duration: 5,
-                type: "video",
-              },
-            ];
-            break;
-          case 1:
-            mediaArray = [
-              {
-                order: 1,
-                path: "staticMedias/English/_DUNK.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 2:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_NOISE.mp4",
-                duration: 6,
-                type: "video",
-              },
-            ];
-            break;
-          case 16:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_1_POINTS.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 17:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_2_POINTS.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 18:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_3_POINTS.mp4",
-                duration: 4,
-                type: "video",
-              },
-            ];
-            break;
-          case 19:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_TIME_OUT.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 20:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_FOUL.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
+        //   case 0:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_DEFENCE.mp4",
+        //         duration: 5,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 1:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_DUNK.mp4",
+        //         duration: 3,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 2:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_NOISE.mp4",
+        //         duration: 6,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 16:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_1_POINTS.mp4",
+        //         duration: 3,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 17:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_2_POINTS.mp4",
+        //         duration: 3,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 18:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_3_POINTS.mp4",
+        //         duration: 4,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 19:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_TIME_OUT.mp4",
+        //         duration: 3,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
+        //   case 20:
+        //     mediaArray = [
+        //       {
+        //         order: 1,
+        //         path: "/medias/English/_FOUL.mp4",
+        //         duration: 3,
+        //         type: "video",
+        //       },
+        //     ];
+        //     break;
           default:
             setMediaMode(true);
             mediaArray = Array.isArray(data.medias)
