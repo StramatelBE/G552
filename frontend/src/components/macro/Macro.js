e65617b10c5d7dd77bcd137dd5b83a1a2cb7d6aa
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-05 12:49:42 +0200

    removed macro id superior to 9

--- frontend/src/components/macro/Macro.js
+++ frontend/src/components/macro/Macro.js
@@ -60,7 +60,7 @@ function Macro() {
     await macroService.update(macro);
   }
 
-  const ignoreMacrosIds = [9];
+  const ignoreMacrosIds = [9, 10, 11, 12, 13, 14, 15];
 
   return (
     <Grid item>
