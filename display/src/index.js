179b6b8626a644e2d3184b32b9226eaa003d68f1
guillaume <legoff.guill@gmail.com>
2024-04-09 10:18:48 +0200

    Supprimer le composant Basketball.jsx de Sports

--- display/src/index.js
+++ display/src/index.js
@@ -5,8 +5,9 @@ import MediaMode from "./Components/MediaMode";
 import config from "./config.js";
 import LogoMode from "./Components/LogoMode";
 import "./main.css"
+import Basketball from "./Components/Sports/Basketball.js";
 
-const { ipcRenderer } = window.require("electron");
+/* const { ipcRenderer } = window.require("electron"); */
 
 const root = document.getElementById("root");
 const appRoot = ReactDOM.createRoot(root);
@@ -17,135 +18,135 @@ const App = () => {
   const [mediaState, setMediaState] = useState([]);
   const [mediaMode, setMediaMode] = useState(false);
 
-  useEffect(() => {
-    document.documentElement.style.setProperty(
-      "--maxWidth",
-      config.display.width
-    );
-    document.documentElement.style.setProperty(
-      "--maxHeight",
-      config.display.height
-    );
-    console.log("App mounted");
-    ipcRenderer.on("server-data", (event, data) => {
-      console.log('!Received gameState', data, event);
-      if (data.Mode === 9) {
-        setMode("scoring");
-        setGameState(data || {}); // Assuming the data for scoring mode contains a 'gameState' property
-      } else if (data.Mode === 22) {
-        setMode("logo");
-      } else if (data.Mode === 23) {
-        setMode("sleep")
-      } else {
-        let mediaArray = [];
-        setMediaMode(false);
-        setMode("media");
-        // if data.medias is not an array, wrap it in one
-        switch (data.Mode) {
-          case 0:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_DEFENCE.mp4",
-                duration: 5,
-                type: "video",
-              },
-            ];
-            break;
-          case 1:
-            mediaArray = [
-              {
-                order: 1,
-                path: "staticMedias/English/_DUNK.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 2:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_NOISE.mp4",
-                duration: 6,
-                type: "video",
-              },
-            ];
-            break;
-          case 16:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_1_POINTS.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 17:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_2_POINTS.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 18:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_3_POINTS.mp4",
-                duration: 4,
-                type: "video",
-              },
-            ];
-            break;
-          case 19:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_TIME_OUT.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          case 20:
-            mediaArray = [
-              {
-                order: 1,
-                path: "/medias/English/_FOUL.mp4",
-                duration: 3,
-                type: "video",
-              },
-            ];
-            break;
-          default:
-            setMediaMode(true);
-            mediaArray = Array.isArray(data.medias)
-              ? data.medias
-              : [data.medias];
-            console.log(mediaArray);
+  /*   useEffect(() => {
+      document.documentElement.style.setProperty(
+        "--maxWidth",
+        config.display.width
+      );
+      document.documentElement.style.setProperty(
+        "--maxHeight",
+        config.display.height
+      );
+      console.log("App mounted");
+      ipcRenderer.on("server-data", (event, data) => {
+        console.log('!Received gameState', data, event);
+        if (data.Mode === 9) {
+          setMode("scoring");
+          setGameState(data || {}); // Assuming the data for scoring mode contains a 'gameState' property
+        } else if (data.Mode === 22) {
+          setMode("logo");
+        } else if (data.Mode === 23) {
+          setMode("sleep")
+        } else {
+          let mediaArray = [];
+          setMediaMode(false);
+          setMode("media");
+          // if data.medias is not an array, wrap it in one
+          switch (data.Mode) {
+            case 0:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_DEFENCE.mp4",
+                  duration: 5,
+                  type: "video",
+                },
+              ];
+              break;
+            case 1:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "staticMedias/English/_DUNK.mp4",
+                  duration: 3,
+                  type: "video",
+                },
+              ];
+              break;
+            case 2:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_NOISE.mp4",
+                  duration: 6,
+                  type: "video",
+                },
+              ];
+              break;
+            case 16:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_1_POINTS.mp4",
+                  duration: 3,
+                  type: "video",
+                },
+              ];
+              break;
+            case 17:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_2_POINTS.mp4",
+                  duration: 3,
+                  type: "video",
+                },
+              ];
+              break;
+            case 18:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_3_POINTS.mp4",
+                  duration: 4,
+                  type: "video",
+                },
+              ];
+              break;
+            case 19:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_TIME_OUT.mp4",
+                  duration: 3,
+                  type: "video",
+                },
+              ];
+              break;
+            case 20:
+              mediaArray = [
+                {
+                  order: 1,
+                  path: "/medias/English/_FOUL.mp4",
+                  duration: 3,
+                  type: "video",
+                },
+              ];
+              break;
+            default:
+              setMediaMode(true);
+              mediaArray = Array.isArray(data.medias)
+                ? data.medias
+                : [data.medias];
+              console.log(mediaArray);
+          }
+          console.log(mediaArray);
+          setMediaState(mediaArray);
         }
-        console.log(mediaArray);
-        setMediaState(mediaArray);
-      }
-    });
-
-    ipcRenderer.on("message", (event, message) => {
-      console.log("Received message:", message);
-    });
-
-    return () => {
-      ipcRenderer.removeAllListeners("server-data");
-    };
-  }, []);
+      });
+  
+      ipcRenderer.on("message", (event, message) => {
+        console.log("Received message:", message);
+      });
+  
+      return () => {
+        ipcRenderer.removeAllListeners("server-data");
+      };
+    }, []); */
 
   return (
     <>
-      {mode === "scoring" &&
+      {/* {mode === "scoring" &&
         <ScoringMode gameState={gameState} />
       }
 
@@ -155,7 +156,8 @@ const App = () => {
       {mode === "logo" && <LogoMode />
       }
       {mode === "sleep" && <></>}
-      {mode === "" && <div>Waiting for data...</div>}
+      {mode === "" && <div>Waiting for data...</div>} */}
+      <Basketball />
     </>
   );
 
