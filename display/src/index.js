1621bc654857bbf9548db07d9cf5c90d16ec51fb
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-16 16:58:28 +0200

    ok

--- display/src/index.js
+++ display/src/index.js
@@ -73,7 +73,7 @@ const App = () => {
         setMode("logo");
       } else if (data.Mode === 23) {
         setMode("sleep")
-      } else {
+      } else if (data.mode === "media") {
         let mediaArray = [];
         setMediaMode(true);
             mediaArray = Array.isArray(data.medias)
