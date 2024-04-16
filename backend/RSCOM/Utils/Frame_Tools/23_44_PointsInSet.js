b4798f24dc27d31636afcfa3178fdb4eaf0487b5
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-16 18:12:12 +0200

    changed pointinset tool logic

--- backend/RSCOM/Utils/Frame_Tools/23_44_PointsInSet.js
+++ backend/RSCOM/Utils/Frame_Tools/23_44_PointsInSet.js
@@ -9,20 +9,18 @@ const nBytesToNumber = require('../nBytesToNumber');
  * @param {Array} message - The byte array containing the points data.
  * @returns {Array} An array containing the points for each set.
  */
-function pointsInSet(startIndex, size, team, message) {
+function pointsInSet(startIndex, size, message) {
     let pointsBySet = new Array(size);
-    let indexOffset = team === "L" ? 0 : 2; // Adjust index based on team (L or V)
 
 
     for (let i = 0; i < message.length; i+=2) {
         console.log(i, nBytesToNumber(message[i], message[i + 1]));
     }
 
-    pointsBySet[0] = nBytesToNumber(message[startIndex + indexOffset], message[startIndex + indexOffset + 1]);
-    pointsBySet[1] = nBytesToNumber(message[startIndex + indexOffset + 4], message[startIndex + indexOffset + 5]);
-    pointsBySet[2] = nBytesToNumber(message[startIndex + indexOffset + 13], message[startIndex + indexOffset + 14]);
-    pointsBySet[3] = nBytesToNumber(message[startIndex + indexOffset + 16], message[startIndex + indexOffset + 17]);
-
+    pointsBySet[0] = nBytesToNumber(message[startIndex], message[startIndex + 1]);
+    pointsBySet[1] = nBytesToNumber(message[startIndex  + 4], message[startIndex + 5]);
+    pointsBySet[2] = nBytesToNumber(message[startIndex + 13], message[startIndex + 14]);
+    pointsBySet[3] = nBytesToNumber(message[startIndex + 16], message[startIndex + 17]);
 
     return pointsBySet;
 }
