f3faaff7122587b371b80b98261fa045426a822b
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-16 18:20:09 +0200

    changed pointinset tool logic

--- display/src/Components/Sports/Tennis/Tennis.js
+++ display/src/Components/Sports/Tennis/Tennis.js
@@ -1,35 +1,40 @@
 import React from "react";
 import "./Tennis.css";
 
-function Tennis({gameState : incomingGameState}) {
+function Tennis({ gameState: incomingGameState }) {
   const gameState = incomingGameState || {};
 
-
-  if  (gameState?.Home?.Points === 17) {
+  // Handle special scoring when points reach 17
+  if (gameState?.Home?.Points === 17) {
     gameState.Home.Points = "A";
     gameState.Guest.Points = "-";
   } else if (gameState?.Guest?.Points === 17) {
     gameState.Guest.Points = "A";
     gameState.Home.Points = "-";
   }
+
+  // Determine the color of the service dot for Home and Guest
+  const homeServiceDotColor = gameState?.Home?.Service === 1 ? "green" : "grey";
+  const guestServiceDotColor = gameState?.Guest?.Service === 1 ? "green" : "grey";
+
   return (
     <div className="scoreboard">
       <div className="player player-bottom">
-      <div className="set-score" style={{ left: "150px" }}>{gameState?.Guest?.GameInSet}</div>
+        <div className="set-score" style={{ left: "150px" }}>{gameState?.Home?.GameInSet}</div>
         <div className="set-score point" style={{ left: "201px" }}>{gameState?.Home?.Points}</div>
         <div className="set-score" style={{ left: "318px" }}>{gameState?.Home?.PointsInSet[0]}</div>
         <div className="set-score" style={{ left: "385px" }}>{gameState?.Home?.PointsInSet[1]}</div>
         <div className="set-score" style={{ left: "452px" }}>{gameState?.Home?.PointsInSet[2]}</div>
-        <div className="dot"></div>
+        <div className="dot" style={{ backgroundColor: homeServiceDotColor }}></div>
         <div className="player-name">{gameState?.Home?.TeamName}</div>
       </div>
       <div className="player player-top">
-      <div className="set-score" style={{ left: "150px" }}>{gameState?.Guest?.GameInSet}</div>
+        <div className="set-score" style={{ left: "150px" }}>{gameState?.Guest?.GameInSet}</div>
         <div className="set-score point" style={{ left: "201px" }}>{gameState?.Guest?.Points}</div>
         <div className="set-score" style={{ left: "318px" }}>{gameState?.Guest?.PointsInSet[0]}</div>
         <div className="set-score" style={{ left: "385px" }}>{gameState?.Guest?.PointsInSet[1]}</div>
         <div className="set-score" style={{ left: "452px" }}>{gameState?.Guest?.PointsInSet[2]}</div>
-        <div className="dot"></div>
+        <div className="dot" style={{ backgroundColor: guestServiceDotColor }}></div>
         <div className="player-name">{gameState?.Guest?.TeamName}</div>
       </div>
 
