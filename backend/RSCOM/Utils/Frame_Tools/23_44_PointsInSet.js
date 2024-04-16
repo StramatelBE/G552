8a92bf11266999edec9647fb6c7758d878043712
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-16 17:58:17 +0200

    changed pointinset tool logic

--- backend/RSCOM/Utils/Frame_Tools/23_44_PointsInSet.js
+++ backend/RSCOM/Utils/Frame_Tools/23_44_PointsInSet.js
@@ -1,22 +1,25 @@
 const nBytesToNumber = require('../nBytesToNumber');
 
-function PointsInSet(startIndex, step, size, _message){
-
+/**
+ * Extract points for either Local (L) or Visitor (V) from the message.
+ * 
+ * @param {number} startIndex - The index in the message where to start reading the points.
+ * @param {number} size - Number of sets to calculate points for.
+ * @param {string} team - Specifies the team, either "L" for Local or "V" for Visitor.
+ * @param {Array} message - The byte array containing the points data.
+ * @returns {Array} An array containing the points for each set.
+ */
+function pointsInSet(startIndex, size, team, message) {
     let pointsBySet = new Array(size);
-
+    let indexOffset = team === "L" ? 0 : 3; // Adjust index based on team (L or V)
 
     for (let i = 0; i < size; i++) {
-        if (i == 2){
-            startIndex += 5
-        }
-
-        pointsBySet[i] = nBytesToNumber(_message[startIndex], _message[startIndex + 1]);
-        startIndex += step;
-
+        // Calculate the current index to read from, adjusting for the set number
+        let currentIndex = startIndex + indexOffset + (i * 5);
+        pointsBySet[i] = nBytesToNumber(message[currentIndex], message[currentIndex + 1]);
     }
-
 
     return pointsBySet;
 }
 
-module.exports = PointsInSet;
+module.exports = pointsInSet;
