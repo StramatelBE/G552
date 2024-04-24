f8105edd81ac9aae71dc5e090953d85ba19673c6
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-24 17:53:39 +0200

    User model folder creation depends on Sport enum like Macro Controller get By Button and User get by Username

--- backend/RSCOM/Game.js
+++ backend/RSCOM/Game.js
@@ -278,9 +278,10 @@ class Game {
       } else {
         // Set default TeamName if not provided or empty
         if (!toInsert[side]) toInsert[side] = {};
+        storage[teamPath] = toInsert[side].TeamName.trim();
+        writeStorage(storage);
         toInsert[side].TeamName = storage[teamPath] && storage[teamPath].trim() ? storage[teamPath] : side;
       }
-      writeStorage(storage);
 
     });
 
