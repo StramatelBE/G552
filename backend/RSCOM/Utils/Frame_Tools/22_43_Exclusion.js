94ee762e46dfb23018c53e0fe870030b8f701645
mderoir <mderoir@PC-MDR.stramatel.local>
2024-04-24 12:08:56 +0200

    color tennis

--- backend/RSCOM/Utils/Frame_Tools/22_43_Exclusion.js
+++ backend/RSCOM/Utils/Frame_Tools/22_43_Exclusion.js
@@ -6,10 +6,10 @@ function Exclusion(startIndex, size, _message, type) {
 
     for (let i = 0; i < size; i++) {
         if (type === 'timer') {
-            let offset = i * 5; // Ajustement pour sauter les 5 octets (3 pour le timer, 2 pour le numéro)
+            let offset = i * 5 + 2; // Ajustement pour sauter les 5 octets (3 pour le timer, 2 pour le numéro)
             result[i] = nBytesToNumber(_message[startIndex + offset], _message[startIndex + offset + 1], _message[startIndex + offset + 2]);
         } else if (type === 'shirtNumber') {
-            let offset = i * 5 + 3; // Ajustement pour accéder aux deux octets du numéro de maillot après les 3 octets de timer
+            let offset = i * 5; // Ajustement pour accéder aux deux octets du numéro de maillot après les 3 octets de timer
             result[i] = nBytesToNumber(_message[startIndex + offset], _message[startIndex + offset + 1]);
         }
     }
