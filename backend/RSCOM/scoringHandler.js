5d28b8005a750dead28e4041f30f71e45c3ce47e
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-04-26 12:31:17 +0200

    condition to chnage storage when empty in frame 0x90

--- backend/RSCOM/scoringHandler.js
+++ backend/RSCOM/scoringHandler.js
@@ -30,7 +30,7 @@ const handleScoring = async (scoring) => {
         const prematchMode = [21];
         const stopModes = [22, 23, 24, 25, 99];
 
-        const getLanguage = await user.getLanguage(scoring.Sport);
+        const getLanguage = await user.getLanguage(scoring.Sport).Language;
 
         // console.log("Handle Scoring:", scoring.Mode)
 
