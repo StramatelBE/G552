ba6e45c915661c994ec3ccf1da803cd65ae2ad1f
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-04-26 11:41:12 +0200

    condition to chnage storage when empty in frame 0x90

--- backend/RSCOM/Game.js
+++ backend/RSCOM/Game.js
@@ -282,10 +282,12 @@ class Game {
       } else { // Retrieve team name
         if (storage[teamPath]) {
           if (!toInsert[side]) toInsert[side] = {}; // Ensure side object exists
-          toInsert[side].TeamName = storage[teamPath];
+          toInsert[side]?.TeamName = storage[teamPath];
         } else if (!toInsert[side]?.TeamName?.trim()) {
           // Default to "Home" or "Guest" if the team name is not in storage and the current is empty
-          toInsert[side].TeamName = side === "Home" ? "Home" : "Guest";
+          if (!toInsert[side]?.TeamName) toInsert[side].TeamName = '';; // Ensure side object exists
+            
+          toInsert[side]?.TeamName = side === "Home" ? "Home" : "Guest";
           storage[teamPath] = toInsert[side].TeamName;
         }
       }
