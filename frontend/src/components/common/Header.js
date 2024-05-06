51d658f7956adf3f5a7f7d444653e7dde95b5e87
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-05-06 09:39:19 +0200

    Mise à jour des traductions et des URL dans le frontend

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,47 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Box, Toolbar } from "@mui/material";
 import { useNavigate } from "react-router-dom"; // Importez useNavigate pour la redirection
+import AdminDialog from "../dialogs/AdminDialog";
 
 function Header(props) {
+  const [modal, setModal] = useState(false);
+  const [adminPassword, setAdminPassword] = useState("");
   const navigate = useNavigate(); // Hook pour naviguer
 
-  // Fonction pour gérer le clic avec les touches Ctrl et Alt enfoncées
+  function toggleModal() {
+    setModal(!modal);
+  }
   const handleLogoClick = (event) => {
-    // Vérifie si les touches Ctrl et Alt étaient enfoncées lors du clic
+
     if (event.ctrlKey && event.altKey) {
-      // Si oui, redirige vers /admin
-      navigate("/admin");
+      toggleModal();
+
     }
   };
+  function adminPasswordCheck() {
+    if (adminPassword === process.env.REACT_APP_PASSWORD_ADMIN) {
+      toggleModal()
+      navigate("/admin");
+      setAdminPassword("");
+    } else {
+      alert("Invalid password");
+    }
+  }
 
   return (
-    <AppBar
-      sx={{
-        justifyContent: "center",
-        position: "sticky",
-        top: "0",
-        marginBottom: "2vh",
-      }}
-    >
-      <Toolbar
-        style={{
+    <>
+      <AppBar
+        sx={{
           justifyContent: "center",
-          padding: "0",
+          position: "sticky",
+          top: "0",
+          marginBottom: "2vh",
         }}
       >
-        <Box
-          component="img"
-          src={
-            props.darkMode
-              ? "/images/Logo_Stramatel_White.png"
-              : "/images/Logo_Stramatel_Dark.png"
-          }
-          alt="Logo"
-          sx={{ width: "200px", height: "auto" }}
-          onClick={handleLogoClick} // Ajouter le gestionnaire d'événements ici
-        />
-      </Toolbar>
-    </AppBar>
+        <Toolbar
+          style={{
+            justifyContent: "center",
+            padding: "0",
+          }}
+        >
+          <Box
+            component="img"
+            src={
+              props.darkMode
+                ? "/images/Logo_Stramatel_White.png"
+                : "/images/Logo_Stramatel_Dark.png"
+            }
+            alt="Logo"
+            sx={{ width: "200px", height: "auto" }}
+            onClick={handleLogoClick} // Ajouter le gestionnaire d'événements ici
+          />
+        </Toolbar>
+      </AppBar>
+      <AdminDialog
+        open={modal}
+        onClose={toggleModal}
+        adminPassword={adminPassword}
+        setAdminPassword={setAdminPassword}
+        onAdd={adminPasswordCheck}
+      />
+    </>
   );
 }
 
---
+++ frontend/src/components/dialogs/AdminDialog.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+function AdminDialog({ open, onClose, onAdd, adminPassword, setAdminPassword }) {
+  const { t } = useTranslation();
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{t("Login.password")}</DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          id="standard-basic"
+          autoComplete="off"
+          value={adminPassword}
+          onChange={(e) => setAdminPassword(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} sx={{ color: "secondary.main" }}>
+          {t("Dialog.cancel")}
+        </Button>
+        <Button
+          onClick={onAdd}
+          sx={{ color: "secondary.main" }}
+
+        >
+          {t("Dialog.confirm")}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default AdminDialog;
