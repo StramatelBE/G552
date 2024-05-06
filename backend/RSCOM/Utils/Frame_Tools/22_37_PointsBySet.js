b09fe268868344ba0bb202c845087dbccbc55a55
MatthieuDeroir <matthieu.deroir@gmail.com>
2024-05-06 16:32:04 +0200

    logging

--- backend/RSCOM/Utils/Frame_Tools/22_37_PointsBySet.js
+++ backend/RSCOM/Utils/Frame_Tools/22_37_PointsBySet.js
@@ -1,18 +1,22 @@
 const nBytesToNumber = require('../nBytesToNumber');
 
-/**
- * Extract points for either Local (L) or Visitor (V) from the message.
- * 
- * @param {number} startIndex - The index in the message where to start reading the points.
- * @param {number} size - Number of sets to calculate points for.
- * @param {string} team - Specifies the team, either "L" for Local or "V" for Visitor.
- * @param {Array} message - The byte array containing the points data.
- * @returns {Array} An array containing the points for each set.
- */
+
 function pointsBySet(startIndex, size, step, message) {
+    // LOG EVERY MESSAGE
+    // Highlight every value that must be in the Array
+    for (let i = 0; i < message.length; i+=2) {
+        console.log(i, nBytesToNumber(message[i], message[i + 1]));
+        console.log(i, message[i], message[i + 1]);
+    }
+    for (let i = 0; i < message.length; i++) {
+        console.log(i, nBytesToNumber(message[i], message[i + 1]));
+        console.log(i, message[i], message[i + 1]);
+    }
+
+
     let pointsBySet = new Array(size);
     for (let i = 0; i < size; i++) {
-        pointsBySet[i] = nBytesToNumber(message[startIndex + step * i ], message[startIndex + i * step + 1]);
+        pointsBySet[i] = nBytesToNumber(message[startIndex + i * step], message[startIndex + i * step + 1]);
     }
     return pointsBySet;
 }
