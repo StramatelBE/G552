09c4936ff8d69fdf72ce8c768f0e590c9f473e2f
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-05-06 11:49:40 +0200

    salu

--- backend/RSCOM/Game.js
+++ backend/RSCOM/Game.js
@@ -7,7 +7,7 @@ const nBytesToNumber = require('./Utils/nBytesToNumber');
 const TeamName = require("./Utils/Frame_Tools/6_48_TeamName");
 class Game {
   static State = {
-    Code : '',
+    Code: '',
     Language: '',
     Mode: '',
 
@@ -128,117 +128,117 @@ class Game {
     let toInsert = null;
 
     switch (_message[1]) {
-        case 0x10:
-            toInsert = Frames.Volleyball.build(_message);
-            console.log("Volleyball Frame");
-            break;
-        case 0x20:
-            toInsert = Frames.Handball.build(_message);
-            console.log("Handball Frame");
-            break;
-        case 0x21:
-            toInsert = Frames.Floorball.build(_message);
-            console.log("Floorball Frame");
-            break;
-        case 0x22:
-            toInsert = Frames.IceHockey.build(_message);
-            console.log("IceHockey Frame");
-            break;
-        case 0x23:
-            toInsert = Frames.RinkHockey.build(_message);
-            console.log("RinkHockey Frame");
-            break;
-        case 0x24:
-            toInsert = Frames.RollerInlineHockey.build(_message);
-            console.log("RollerInlineHockey Frame");
-            break;
-        case 0x25:
-            toInsert = Frames.Futsal.build(_message);
-            console.log("Futsal Frame");
-            break;
-        case 0x26:
-            toInsert = Frames.Netball.build(_message);
-            console.log("Netball Frame");
-            break;
-        case 0x27:
-            toInsert = Frames.Boxe.build(_message);
-            console.log("Boxe Frame");
-            break;
-        case 0x30:
-            toInsert = Frames.Basketball.build(_message);
-            console.log("Basketball Frame");
-            break;
-        case 0x40:
-            toInsert = Frames.Tennis.build(_message);
-            console.log("Tennis Frame");
-            break;
-        case 0x41:
-            toInsert = Frames.Badminton.build(_message);
-            console.log("Badminton Frame");
-            break;
-        case 0x42:
-            toInsert = Frames.TableTennis.build(_message);
-            console.log("TableTennis Frame");
-            break;
-        case 0x50:
-            toInsert = Frames.Chrono.build(_message);
-            console.log("Chrono Frame");
-            break;
-        case 0x51:
-            toInsert = Frames.Training.build(_message);
-            console.log("Training Frame");
-            break;
-        case 0x52:
-            toInsert = Frames.FreeSport.build(_message);
-            console.log("FreeSport Frame");
-            break;
-        case 0x90:
-            toInsert = Frames.TeamNames.build(_message);
-            console.log("TeamNames Frame");
-            break;
-        case 0x91:
-            toInsert = Frames.ClearTeamNames.build(_message);
-            console.log("ClearTeamNames Frame");
-            break;
-        case 0x92:
-            toInsert = Frames.FullClear.build(_message);
-            console.log("FullClear Frame");
-            break;
-        case 0x93:
-            toInsert = Frames.Test.build(_message);
-            console.log("Test Frame");
-            break;
-        case 0x99:
-            toInsert = Frames.ClockSetup.build(_message);
-            console.log("ClockSetup Frame");
-            break;
-        default:
-            console.log("Unknown Frame: " + _message[1]);
-            break;
+      case 0x10:
+        toInsert = Frames.Volleyball.build(_message);
+        console.log("Volleyball Frame");
+        break;
+      case 0x20:
+        toInsert = Frames.Handball.build(_message);
+        console.log("Handball Frame");
+        break;
+      case 0x21:
+        toInsert = Frames.Floorball.build(_message);
+        console.log("Floorball Frame");
+        break;
+      case 0x22:
+        toInsert = Frames.IceHockey.build(_message);
+        console.log("IceHockey Frame");
+        break;
+      case 0x23:
+        toInsert = Frames.RinkHockey.build(_message);
+        console.log("RinkHockey Frame");
+        break;
+      case 0x24:
+        toInsert = Frames.RollerInlineHockey.build(_message);
+        console.log("RollerInlineHockey Frame");
+        break;
+      case 0x25:
+        toInsert = Frames.Futsal.build(_message);
+        console.log("Futsal Frame");
+        break;
+      case 0x26:
+        toInsert = Frames.Netball.build(_message);
+        console.log("Netball Frame");
+        break;
+      case 0x27:
+        toInsert = Frames.Boxe.build(_message);
+        console.log("Boxe Frame");
+        break;
+      case 0x30:
+        toInsert = Frames.Basketball.build(_message);
+        console.log("Basketball Frame");
+        break;
+      case 0x40:
+        toInsert = Frames.Tennis.build(_message);
+        console.log("Tennis Frame");
+        break;
+      case 0x41:
+        toInsert = Frames.Badminton.build(_message);
+        console.log("Badminton Frame");
+        break;
+      case 0x42:
+        toInsert = Frames.TableTennis.build(_message);
+        console.log("TableTennis Frame");
+        break;
+      case 0x50:
+        toInsert = Frames.Chrono.build(_message);
+        console.log("Chrono Frame");
+        break;
+      case 0x51:
+        toInsert = Frames.Training.build(_message);
+        console.log("Training Frame");
+        break;
+      case 0x52:
+        toInsert = Frames.FreeSport.build(_message);
+        console.log("FreeSport Frame");
+        break;
+      case 0x90:
+        toInsert = Frames.TeamNames.build(_message);
+        console.log("TeamNames Frame");
+        break;
+      case 0x91:
+        toInsert = Frames.ClearTeamNames.build(_message);
+        console.log("ClearTeamNames Frame");
+        break;
+      case 0x92:
+        toInsert = Frames.FullClear.build(_message);
+        console.log("FullClear Frame");
+        break;
+      case 0x93:
+        toInsert = Frames.Test.build(_message);
+        console.log("Test Frame");
+        break;
+      case 0x99:
+        toInsert = Frames.ClockSetup.build(_message);
+        console.log("ClockSetup Frame");
+        break;
+      default:
+        console.log("Unknown Frame: " + _message[1]);
+        break;
     }
 
     if (toInsert != null) {
       toInsert.Code = _message[1];
 
-        console.log(toInsert.Sport);
+      console.log(toInsert.Sport);
 
       if (toInsert?.Guest?.Exclusion?.Timer) {
         if (toInsert?.Home?.Exclusion?.Timer) {
-            for (let i = 0; i < toInsert.Home.Exclusion.Timer.length; i++) {
-                console.log(i, ".Home Exclusion Timer: ", toInsert.Home.Exclusion.Timer[i]);
-            }
-        for (let i = 0; i < toInsert.Guest.Exclusion.Timer.length; i++) {
-          console.log(i, ".Guest Exclusion Timer: ", toInsert.Guest.Exclusion.Timer[i]);
+          for (let i = 0; i < toInsert.Home.Exclusion.Timer.length; i++) {
+            console.log(i, ".Home Exclusion Timer: ", toInsert.Home.Exclusion.Timer[i]);
+          }
+          for (let i = 0; i < toInsert.Guest.Exclusion.Timer.length; i++) {
+            console.log(i, ".Guest Exclusion Timer: ", toInsert.Guest.Exclusion.Timer[i]);
+          }
         }
+
       }
-     
-        }
 
       console.log(nBytesToNumber(_message[1]) + " Frame");
       this.updateState(toInsert);
-      
+
     }
-    this.Send();     
+    this.Send();
   };
 
   static getState() {
@@ -247,7 +247,7 @@ class Game {
 
   static updateState(toInsert) {
     const storagePath = './storage.json';
-  
+
     // Function to read the current storage state
     const readStorage = () => {
       try {
@@ -258,7 +258,7 @@ class Game {
         return {};
       }
     };
-  
+
     // Function to write to the storage
     const writeStorage = (data) => {
       try {
@@ -267,37 +267,37 @@ class Game {
         console.error('Error writing to storage:', err);
       }
     };
-  
+
     // Load existing storage to handle updates or retrievals
     const storage = readStorage();
-  
+
     ['Guest', 'Home'].forEach(side => {
       const teamPath = `${side}.TeamName`;
       if (!toInsert[side]) {
         toInsert[side] = {}; // Initialize the side object if it doesn't exist
-    }
-  
+      }
+
       // Check the frame code to determine if we should store or retrieve the team name
       if (toInsert.Code === 0x90) { // Store team name
-        
-          storage[teamPath] = toInsert[side].TeamName.trim();
-        
+
+        storage[teamPath] = toInsert[side].TeamName.trim();
+
       } else { // Retrieve team name
         if (storage[teamPath]) {
           if (!toInsert[side]) toInsert[side] = {}; // Ensure side object exists
-          toInsert[side].TeamName = storage[teamPath];
+          toInsert[side]?.TeamName = storage[teamPath];
         } else if (!toInsert[side]?.TeamName?.trim()) {
           // Default to "Home" or "Guest" if the team name is not in storage and the current is empty
           if (!toInsert[side]?.TeamName) toInsert[side].TeamName = ''; // Ensure side object exists
-            
+
           toInsert[side].TeamName = side === "Home" ? "Home" : "Guest";
           storage[teamPath] = toInsert[side].TeamName;
         }
       }
     });
-  
+
     writeStorage(storage);
-  
+
     // Now perform the recursive update
     const recursiveUpdate = (mainObject, updateObject) => {
       for (let key in updateObject) {
@@ -309,11 +309,11 @@ class Game {
         }
       }
     };
-  
+
     recursiveUpdate(this.State, toInsert);
   }
-  
-  
+
+
 
 
   static Send() {
