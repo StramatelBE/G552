688dc07d8b412d47d9a111eda02ecb48789ed822
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-05-13 16:54:34 +0200

    chore: Update volleyball component styling and timeout dot position

--- display/src/Components/Sports/Volleyball/Volleyball.js
+++ display/src/Components/Sports/Volleyball/Volleyball.js
@@ -111,7 +111,7 @@ function Volleyball({ gameState: incomingGameState }) {
         <div className="text team-name-sport" style={{ fontSize: homeFontSize, left: "0px", top: "90px" }} >
           {gameState?.Home?.TeamName !== undefined ? gameState?.Home?.TeamName : "HOME"}
         </div>
-        <div className="side-numbers" style={{ left: "0px", top: "140px" }}>
+        <div className="side-numbers" style={{ left: "0px", top: "150px" }}>
           {gameState && Object.values(gameState.Home.PointsBySet).map((point, index) => {
             const isLosing = gameState.Guest.PointsBySet[index] < point;
             if (isLosing && (point !== 0 || gameState.Guest.PointsBySet[index] !== 0)) {
@@ -119,17 +119,18 @@ function Volleyball({ gameState: incomingGameState }) {
             }
           })}
 
-          {gameState?.Home?.Timeout?.Count >= 0 && (
-            [...Array(gameState?.Home?.Timeout?.Count)].map((_, index) => (
-              <div
-                key={index}
-                className="timeout-dot-sport"
-                style={{ left: "110px", top: `${20 + index * 25}px` }}
-              />
-            ))
-          )}
+
 
         </div>
+        {gameState?.Home?.Timeout?.Count >= 0 && (
+          [...Array(gameState?.Home?.Timeout?.Count)].map((_, index) => (
+            <div
+              key={index}
+              className="timeout-dot-sport"
+              style={{ left: "110px", top: `${150 + index * 25}px` }}
+            />
+          ))
+        )}
       </div>
       <div className="container-team-sport" style={{ left: "286px" }}>
         <div className="container-score" style={{ right: "0px", top: "0px" }} >
@@ -146,7 +147,7 @@ function Volleyball({ gameState: incomingGameState }) {
         <div className="text team-name-sport" style={{ fontSize: homeFontSize, left: "0px", top: "90px", fontSize: guestFontSize }} >
           {gameState?.Guest?.TeamName !== undefined ? gameState?.Guest?.TeamName : "GUEST"}
         </div>
-        <div className="side-numbers" style={{ right: "0px", top: "140px" }}>
+        <div className="side-numbers" style={{ right: "0px", top: "150px" }}>
           {gameState && Object.values(gameState.Guest.PointsBySet).map((point, index) => {
             const isLosing = gameState.Home.PointsBySet[index] < point;
             if (isLosing && (point !== 0 || gameState.Home.PointsBySet[index] !== 0)) {
@@ -155,16 +156,17 @@ function Volleyball({ gameState: incomingGameState }) {
             { isLosing }
           })}
 
-          {gameState?.Guest?.Timeout?.Count >= 0 && (
-            [...Array(gameState?.Guest?.Timeout?.Count)].map((_, index) => (
-              <div
-                key={index}
-                className="timeout-dot-sport"
-                style={{ right: "110px", top: `${20 + index * 25}px` }}
-              />
-            ))
-          )}
+
         </div>
+        {gameState?.Guest?.Timeout?.Count >= 0 && (
+          [...Array(gameState?.Guest?.Timeout?.Count)].map((_, index) => (
+            <div
+              key={index}
+              className="timeout-dot-sport"
+              style={{ right: "110px", top: `${150 + index * 25}px` }}
+            />
+          ))
+        )}
       </div>
 
 
