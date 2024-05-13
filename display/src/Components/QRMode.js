96c83b786f67795bfa2e2b37150e211d30874afe
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-05-13 14:22:54 +0200

    QRMode update

--- display/src/Components/QRMode.js
+++ display/src/Components/QRMode.js
@@ -4,10 +4,14 @@ import "./Mode.css";
 const LogoMode = () => {
     return (
         <div className="container">
-             <iframe src="http://localhost:4000/qrcode" title="QR Code" />
-             <img
+            <iframe 
+                className="logo-iframe" 
+                src="http://localhost:4000/qrcode" 
+                title="QR Code"
+            />
+            <img
                 src="http://localhost:3000/medias/Logo/LOGO_Stramatel.gif"
-
+                alt="Logo"
             />
         </div>
     );
