209aa73bea639ed0ce97494e63a3947dfe8ead49
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-05-15 14:34:12 +0200

    ok

--- display/src/Components/Sports/Tennis/Tennis.js
+++ display/src/Components/Sports/Tennis/Tennis.js
@@ -74,7 +74,7 @@ function Tennis({ gameState: incomingGameState }) {
         </div>
       </td>
     ));
-    if (currentSet > 4 || (gameState?.Home?.Winner || gameState?.Guest?.Winner)) {
+    if (currentSet > 4 || (currentSet === 4 && (gameState?.Home?.Winner || gameState?.Guest?.Winner))) {
       sets.push(
         <td key={3} className="set-cell">
           <div style={{ fontSize: getFontSizeScore(pointsInSet[3] || 0) }} className="set-score">
